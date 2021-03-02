970ae1fcb59816be15e5e461cac3b46f14f85dd5
Joey Rubas <46765074+JoeyRubas@users.noreply.github.com>
2021-03-02 14:40:55 -0600

    Update the rework to work with insta data download refactor

--- insta_rework.py
+++ insta_rework.py
@@ -1,41 +1,78 @@
+from textblob import TextBlob 
+import json
+import os
+
 class person():
     def __init__(self, name):
         self.name = name
         self.words = []
-        self.likes = 0
         self.messages = 0
+        self.message_blocks = 0
+        self.likes_given = 0
+        self.sentiment = 0
+        self.photos = 0
+        self.likes_given = 0
+        self.likes = 0
+        self.video_calls = 0
     def __str__(self):
-        return self.name + ": Words: " + str(self.get_count()) + " Messages: " + str(self.messages) + " Likes: " + str(self.likes)
+        return self.name + ": Words: " + str(self.get_count()) + " Messages: " + str(self.messages)
 
     def add_word(self, word):
         self.words.append(word)
-        
-    def add_like(self):
-        self.likes +=1
-        
+
     def add_message(self):
-        self.messages +=1
+        self.messages += 1
+
+    def add_message_block(self):
+        self.message_blocks +=1
+        
+    def add_photo(self):
+        self.photos += 1
+
+    def add_like(self):
+        self.likes += 1
+
+    def add_like_given(self):
+        self.likes_given += 1
+
+    def add_video_call(self):
+        self.video_calls += 1
         
     def get_name(self):
         return self.name
     
     def get_count(self):
         return len(self.words)
-
-    def get_likes(self):
-        return self.likes
               
     def get_messages(self):
         return self.messages
 
+    def get_message_blocks(self):
+        return self.message_blocks
+
+    def get_likes(self):
+        return self.likes
+
+    def get_likes_given(self):
+        return self.likes_given
+    
+    def get_photos(self):
+        return self.photos
+    
     def get_word_len(self):
         total = 0
         for word in self.words:
             total += len(word)
-        return total/len(self.words)
+        return total/(len(self.words)+1)
 
     def get_message_len(self):
-        return len(self.words)/self.messages
+        return len(self.words)/(self.messages + 1)
+
+    def get_message_blocks_len(self):
+        return len(self.words)/( self.message_blocks + 1)
+
+    def get_video_calls(self):
+        return self.video_calls
 
     def get_most_common(self):
         dic = {}
@@ -48,83 +85,180 @@ class person():
         for word in dic:
             lis.append((dic[word], word))
         lis.sort(reverse=True)
-        return lis[0][1]
+        try:
+            return lis[0][1]
+        except:
+            return ""
 
-tags = ["sammysharp1", "emilie_casey", "Paurecio", "Alyssa.agnos", "Haaris.khannn", "Itscameronrozek", "A.y.z_123", "Abby.fishh", "Kkatiedalton", "Comer_chris2", "Kzcheng1", "Nick.innis8",
-          "Elizabethshamoun", "Johnyboii101", "Realhenryh", "Harvey_wang_", "Ev_k03", "Jack_vest_", "Connor_mchugh12",  "K_bagchi", "Uju.kim", "Evan._.liu", "joey_rubas", "Williamtongit",
-          "Orrsoarsororesormore", "Cooperholmberg", "paige_lafferty", "Eileensmithh", "Morgan.sokol", "Paige_kolbe", "Abby.christman", "arja_500", "kassidyxy", "zoe.k918", "daniel.wu042",
-        "ryanmadson01", "maxicat713", "alnass.ramy", "prem_chandraracecar"]
-for num in range(len(tags)):
-    tags[num] = tags[num].lower()
+    def get_word_sentiment(self):
 
-people = {}
-dic = {}
-for tag in tags:
-    people[tag] = person(tag)
-    
-file = open("doc.txt", "r")
-prev = 0
-last = "joey_rubas"
-current = "joey_rubas"
-total = 0
-for line in file:
-    for word in line.strip().split():
-        word = word.lower()
-        if word in people:
-            current = word
-            if prev and word != last:
-                people[last].add_like()
-                
-            elif word != last: 
-                people[word].add_message()
-                last = word
-                prev = 1
+        if self.sentiment:
+            return self.sentiment
         else:
-            total +=1
-            prev = 0
-            people[current].add_word(word)
-            if word in dic:
-                dic[word] += 1
-            else:
-                dic[word] = 1
+            su = 0
+            total = 0
+            for word in self.words:
+                temp = TextBlob(word).sentiment.polarity
+                if temp:
+                    su += temp
+                    total += 1
+            try:
+                self.sentiment = su/total
+            except:
+                pass
+            return self.sentiment
+
+
+
+
+    
+
+
+
+
 lis = []
-for person in people:
-    lis.append(people[person])
+indicies = []
+dic = {}
+
+
+
+for user in ["Joey Rubas"]:
+    print(user)
+    lis.append(person(user))
+    indicies.append(user)
+    
+
+
+new = 0
+current = lis[0].get_name()
+total = 0
+
+directory_contents = os.listdir(".")
+
+for item in ["folder name"]: #replace with directory_contents to do all folders
+     if os.path.isdir(item):
+        dataIn = json.load(open(item+"/message_1.json", "r"))
+        for message in dataIn["messages"]:
+            if message["sender_name"] != current:
+                lis[indicies.index(current)].add_message_block()
+                if not(message["sender_name"] in indicies):
+                    #print("here", message["sender"])
+                    lis.append(person(message["sender_name"]))
+                    indicies.append( message["sender_name"])
+                current = message["sender_name"]
+            lis[indicies.index(current)].add_message()
+            try:
+                if "liked" in message["content"] or "Liked" in message["content"] or "Video call ended" in message["content"]:
+                        message["content"] = ""
+                if "Video call started" in message["content"]:
+                        lis[indicies.index(current)].add_video_call()
+                        message["content"] = ""
+                for word in message["content"].strip().split():
+                    fixed = word.replace("?", "").replace(".", "").replace("!", "")
+                    lis[indicies.index(current)].add_word(fixed)
+                    total += 1
+                    if word in dic:
+                        dic[fixed] += 1
+                    else:
+                        dic[fixed] = 1
+            except Exception as e:
+                lis[indicies.index(current)].add_photo()
+            try:
+                for like in message["reactions"]:
+                    lis[indicies.index(current)].add_like()
+                    lis[indicies.index(like["actor"])].add_like_given()
+            except Exception as e:
+                pass
+                  
+        
+
+
 
 print("\n\nSummary")
-lis.sort(key = lambda x: x.get_count(), reverse=True)
-for person in lis:
-    print(person)
 
+words = 0
+messages = 0
+mbs = 0
+photos = 0
+likes = 0
+for person in lis:
+    words+= person.get_count()
+    messages += person.get_messages()
+    mbs += person.get_message_blocks()
+    photos += person.get_photos()
+    likes += person.get_likes()
+print("Total Words:", words)
+print("Total messages:", messages)               
+print("Total Message Bloacks:", mbs)
+print("Total Photos:", photos)
+print("Total Likes:", likes)
 
 print("\n\nWords Leaderboard")
 lis.sort(key = lambda x: x.get_count(), reverse=True)
 for num in range(len(lis)):
-    print(num, ".", lis[num].get_name(), ":",  lis[num].get_count())
+    print(num +1, ".", lis[num].get_name(), ":",  lis[num].get_count())
 
 print("\n\nMessages Leaderboard")
 lis.sort(key = lambda x: x.get_messages(), reverse=True)
 for num in range(len(lis)):
-    print(num, ".", lis[num].get_name(), ":",  lis[num].get_messages())
+    print(num +1, ".", lis[num].get_name(), ":",  lis[num].get_messages())
+               
+print("\n\nMessage Blocks Leaderboard")
+lis.sort(key = lambda x: x.get_message_blocks(), reverse=True)
+for num in range(len(lis)):
+    print(num +1, ".", lis[num].get_name(), ":",  lis[num].get_message_blocks())
+
+print("\n\nPhotos Leaderboard")
+lis.sort(key = lambda x: x.get_photos(), reverse=True)
+for num in range(len(lis)):
+    print(num +1, ".", lis[num].get_name(), ":",  lis[num].get_photos())
 
 print("\n\nLikes Leaderboard")
 lis.sort(key = lambda x: x.get_likes(), reverse=True)
 for num in range(len(lis)):
-    print(num, ".", lis[num].get_name(), ":",  lis[num].get_likes())
+    print(num + 1, ".", lis[num].get_name(), ":",  lis[num].get_likes())
+
+print("\n\nLikes Given Leaderboard")
+lis.sort(key = lambda x: x.get_likes_given(), reverse=True)
+for num in range(len(lis)):
+    print(num +1, ".", lis[num].get_name(), ":",  lis[num].get_likes_given())
+
+print("\n\nVideo Calls Leaderboard")
+lis.sort(key = lambda x: x.get_video_calls(), reverse=True)
+for num in range(len(lis)):
+    print(num +1, ".", lis[num].get_name(), ":",  lis[num].get_video_calls())
+
+print("\n\nNiceness Leaderboard")
+lis.sort(key = lambda x: x.get_word_sentiment(), reverse=True)
+for num in range(len(lis)):
+    print(num + 1, ".", lis[num].get_name(), ":",  lis[num].get_word_sentiment())
+
+print("\n\nMeanness Leaderboard")
+lis.sort(key = lambda x: x.get_word_sentiment())
+for num in range(len(lis)):
+    print(num + 1, ".", lis[num].get_name(), ":",  lis[num].get_word_sentiment())
 
 print("\n\nWord Length Leaderboard")
 lis.sort(key = lambda x: x.get_word_len(), reverse=True)
 for num in range(len(lis)):
-    print(num, ".", lis[num].get_name(), ":",  lis[num].get_word_len())
+    print(num + 1, ".", lis[num].get_name(), ":",  lis[num].get_word_len())
 
 print("\n\nMessage Length Leaderboard")
 lis.sort(key = lambda x: x.get_message_len(), reverse=True)
 for num in range(len(lis)):
-    print(num, ".", lis[num].get_name(), ":",  lis[num].get_message_len())
-        
+    print(num + 1, ".", lis[num].get_name(), ":",  lis[num].get_message_len())
+               
+print("\n\nMessage Block Length Leaderboard")
+lis.sort(key = lambda x: x.get_message_blocks_len(), reverse=True)
+for num in range(len(lis)):
+    print(num + 1, ".", lis[num].get_name(), ":",  lis[num].get_message_blocks_len())
+
 print("\n\nMost Common Word by Person")
 for num in range(len(lis)):
-    print(lis[num].get_name(), ":",  lis[num].get_most_common())
+    try:
+        print(lis[num].get_name(), ":",  lis[num].get_most_common())
+    except:
+        pass
 
 print("\n\nMost common words")
 
@@ -136,4 +270,4 @@ list1.reverse()
 for word in list1[:35]:
     print(word[1], ":", word[0], " times")
 
-    
+
